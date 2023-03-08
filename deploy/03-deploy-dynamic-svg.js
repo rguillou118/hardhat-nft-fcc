4b11d52d5d73342e7af01860c25c3f75d394032d
rguillou118 <rguillou118@gmail.com>
2023-03-08 17:25:20 +0100

    first commit

---
+++ deploy/03-deploy-dynamic-svg.js
@@ -0,0 +1,36 @@
+const { network, ethers } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+const fs = require("fs")
+
+module.exports = async ({ getNamedAccounts, deployments }) => {
+    const { deploy, log } = deployments
+    const { deployer } = await getNamedAccounts()
+
+    const chainId = network.config.chainId
+    let ethUsdPriceFeedAddress
+
+    if (developmentChains.includes(network.name)) {
+        const EthUsdAggregator = await ethers.getContract("MockV3Aggregator")
+        ethUsdPriceFeedAddress = EthUsdAggregator.address
+    } else {
+        ethUsdPriceFeedAddress = network.config[chainId].ethUsdPriceFeed
+    }
+    log("-------------------------------------")
+    const lowSVG = await fs.readFileSync("./images/dynamicNft/frown.svg", { encoding: "utf8" })
+    const highSVG = await fs.readFileSync("./images/dynamicNft/happy.svg", { encoding: "utf8" })
+    args = [ethUsdPriceFeedAddress, lowSVG, highSVG]
+    const dynamicSvgNft = await deploy("DynamicSvgNft", {
+        from: deployer,
+        args: args,
+        log: true,
+        waitConfirmations: network.config.blockConfirmations || 1,
+    })
+
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(dynamicSvgNft.address, this.args)
+    }
+}
+
+module.exports.tags = ["all", "dynamicsvg", "main"]
---
+++ deploy/04-mint.js
@@ -0,0 +1,41 @@
+const { network, ethers } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+
+module.exports = async ({ getNamedAccounts }) => {
+    const { deployer } = await getNamedAccounts()
+
+    // Basic NFT
+    const basicNft = await ethers.getContract("BasicNft", deployer)
+    const basicMintTx = await basicNft.mintNft()
+    await basicMintTx.wait(1)
+    console.log(`Basic NFT index 0 has tokenURI: ${await basicNft.tokenURI(0)}`)
+
+    // Random IPFS NFT
+    const randomIpfsNft = await ethers.getContract("RandomIpfsNft", deployer)
+    const mintFee = await randomIpfsNft.getMintFee()
+
+    await new Promise(async (resolve, reject) => {
+        setTimeout(resolve, 300000) // 5 minutes
+        randomIpfsNft.once("NftMinted", async function () {
+            resolve()
+        })
+
+        const randomIpfsMintTx = await randomIpfsNft.requestNft({ value: mintFee.toString() })
+        const randomIpfsMintTxReceipt = await randomIpfsMintTx.wait(1)
+        if (developmentChains.includes(network.name)) {
+            const requestId = randomIpfsMintTxReceipt.events[1].args.requestId.toString()
+            const vrfCoordinatorV2Mock = await ethers.getContract("VRFCoordinatorV2Mock", deployer)
+            await vrfCoordinatorV2Mock.fulfillRandomWords(requestId, randomIpfsNft.address)
+        }
+    })
+    console.log(`Random IPFS NFT index 0 tokenURI: ${await randomIpfsNft.tokenURI(0)}`)
+
+    // Dynamic SVG
+    const highValue = ethers.utils.parseEther("4000")
+    const dynamicSvgNft = await ethers.getContract("DynamicSvgNft", deployer)
+    const dynamicSvgMintTx = await dynamicSvgNft.mintNft(highValue.toString())
+    await dynamicSvgMintTx.wait(1)
+    console.log(`Dynamic SVG NFT index 0 tokenURI: ${await dynamicSvgNft.tokenURI(0)}`)
+}
+
+module.exports.tags = ["all", "mint"]
